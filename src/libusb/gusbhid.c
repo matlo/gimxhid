4a0959df7e3f83d724cf888c027b3e9174ff92ba
matlo <mat.lau@laposte.net>
2019-09-19 13:04:54 +0200

    Remove libusb dependency from gimxcommon/include/gerror.h.

--- src/libusb/gusbhid.c
+++ src/libusb/gusbhid.c
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016 Mathieu Laurendeau <mat.lau@laposte.net>
+ Copyright (c) 2019 Mathieu Laurendeau <mat.lau@laposte.net>
  License: GPLv3
  */
 
@@ -56,6 +56,23 @@ struct ghid_device {
 GLIST_INST(struct ghid_device, usbhid_devices, ghid_close)
 
 static unsigned int clients = 0;
+
+#if !defined(LIBUSB_API_VERSION) && !defined(LIBUSBX_API_VERSION)
+static const char * LIBUSB_CALL libusb_strerror(enum libusb_error errcode)
+{
+  return libusb_error_name(errcode);
+}
+#endif
+
+#define PRINT_ERROR_LIBUSB(libusbfunc,ret) \
+  if (GLOG_LEVEL(GLOG_NAME,ERROR)) { \
+    fprintf(stderr, "%s:%d %s: %s failed with error: %s\n", __FILE__, __LINE__, __func__, libusbfunc, libusb_strerror(ret)); \
+  }
+
+#define PRINT_TRANSFER_ERROR(transfer) \
+  if (GLOG_LEVEL(GLOG_NAME,ERROR)) { \
+    fprintf(stderr, "libusb_transfer failed with status %s (endpoint=0x%02x)\n", libusb_error_name(transfer->status), transfer->endpoint); \
+  }
 
 #define CHECK_INITIALIZED(RETVALUE) \
     if (clients == 0) { \
